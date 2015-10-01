42ddca0c38781fb8a56dbdb9720ea6c035f007cb
Jessamyn Smith <geekchick77@gmail.com>
2015-09-30 22:01:57 -0400

    Added ng-admin

--- www/js/app.js
+++ www/js/app.js
@@ -5,28 +5,34 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // the 2nd parameter is an array of 'requires'
 //var app = angular.module('angularStarterProject', ['ui.router', 'angularStarterProject.controllers'])
-angular.module('angularStarterProject', ['ui.router', 'angularStarterProject.controllers'])
+angular.module('angularStarterProject', ['ui.router', 'ng-admin', 'angularStarterProject.controllers'])
 
-    .config(function() {
-      // Add configuration here
-    })
+  .config(['NgAdminConfigurationProvider', function(NgAdminConfigurationProvider) {
+    var nga = NgAdminConfigurationProvider;
+    // create an admin application
+    var admin = nga.application('My First Admin');
+    // more configuation here later
+    // ...
+    // attach the admin application to the DOM and run it
+    nga.configure(admin);
+  }])
 
-    .run(function() {
-      // Add run code here
-    })
+  .run(function() {
+    // Add run code here
+  })
 
-    .config(function($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
-      $stateProvider
+    $stateProvider
 
-        .state('home', {
-          url: '/home',
-          templateUrl: 'templates/home.html',
-          controller: 'HomeCtrl'
-        });
+      .state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      });
 
-      // if none of the above states are matched, use this as the fallback
-      $urlRouterProvider.otherwise('/home');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/home');
 
-    })
-  ;
+  })
+;
