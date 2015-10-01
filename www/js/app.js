7ff80e8999dc4256a63c930e42e81489595317b3
Jessamyn Smith <geekchick77@gmail.com>
2015-10-01 00:42:14 -0400

    Added sorting of items

--- www/js/app.js
+++ www/js/app.js
@@ -38,7 +38,70 @@ angular.module('ngAdminFirebase', ['ng-admin'])
     nga.configure(admin);
   }])
 
-  .run(function() {
-    // Add run code here
-  })
+  .config(['RestangularProvider', function(RestangularProvider) {
+    RestangularProvider.addFullRequestInterceptor(function(element, operation, what, url, headers, params) {
+      // The firebase api has an unusual interface, so this may not be helpful. Leaving it here for reference.
+      if (operation == "getList") {
+        //// custom sort params
+        //if (params._sortField && params._sortField == "name") {
+        //  params.orderBy = '\"' + params._sortField + '\"';
+        //  params.startAt = '\"A\"';
+        //}
+        //delete params._sortField;
+        //delete params._sortDir;
+        //
+        //// custom pagination params
+        //if (params._page && params.orderBy && params.orderBy == "id") {
+        //  params.startAt = (params._page - 1) * params._perPage;
+        //  params.endAt = params._page * params._perPage;
+        //}
+        //delete params._page;
+        //delete params._perPage;
+        //
+        //// custom filters
+        //if (params._filters) {
+        //  for (var filter in params._filters) {
+        //    params[filter] = params._filters[filter];
+        //  }
+        //}
+        //delete params._filters;
+      }
+      return {params: params};
+    });
+
+    RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+      var results = data;
+      if (Object.prototype.toString.call(results) !== '[object Array]') {
+        // transform object to array
+        results = [];
+        for (var property in data) {
+          if (data.hasOwnProperty(property)) {
+            results.push(data[property]);
+          }
+        }
+      }
+
+      var sortField = response.config.params["_sortField"];
+      var sortDirection = response.config.params["_sortDir"];
+      var lessThanResult = -1;
+      var greaterThanResult = 1;
+      if (sortDirection == "DESC") {
+        lessThanResult = 1;
+        greaterThanResult = -1;
+      }
+      var compareSortFields = function(a, b) {
+        if (a[sortField] < b[sortField])
+          return lessThanResult;
+        if (a[sortField] > b[sortField])
+          return greaterThanResult;
+        return 0;
+      };
+
+      if (results[0].hasOwnProperty(sortField)) {
+        results.sort(compareSortFields);
+      }
+
+      return results;
+    });
+  }])
 ;
